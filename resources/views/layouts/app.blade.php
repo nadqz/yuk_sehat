6644891a0fac2bcea8fc24d8d10e7c6902129ba1
NuttQS <106371427+nadqz@users.noreply.github.com>
2025-12-23 21:48:14 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="manifest" href="{{ secure_asset('manifest.json') }}" crossorigin="use-credentials">
